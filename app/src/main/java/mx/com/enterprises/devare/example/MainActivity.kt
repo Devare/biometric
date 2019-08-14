bbcabe5daef2186f736e434d0a4f36a12150462e
jafh <ing_angelfierro@hotmail.com>
2019-08-13 23:44:55 -0500

    Primer commit del proyecto

---
+++ app/src/main/java/mx/com/enterprises/devare/example/MainActivity.kt
@@ -0,0 +1,13 @@
+package mx.com.enterprises.devare.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.com.enterprises.devare.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
